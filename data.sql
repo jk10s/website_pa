f086536bb5c2dcc237393d691c2199c0d17c0cf5
jk10s <87342758+jk10s@users.noreply.github.com>
2023-09-07 11:30:47 -0500

    Update data.sql
    upaktae

--- data.sql
+++ data.sql
@@ -54,3 +54,76 @@ CREATE TABLE Reportes (
     TotalVentas DECIMAL(10, 2) NOT NULL,
     OtrosDetalles TEXT
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE IF NOT EXISTS discotelapatrona;
+
+USE discotelapatrona;
+
+-- Tabla Empleados
+CREATE TABLE IF NOT EXISTS Empleados (
+    IDEmpleado INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    TipoCuenta ENUM('administrador','empleado') NOT NULL,
+    EsAdministrador TINYINT(1) NOT NULL DEFAULT 0
+);
+
+-- Tabla InventarioProductos
+CREATE TABLE IF NOT EXISTS InventarioProductos (
+    IDProducto INT AUTO_INCREMENT PRIMARY KEY,
+    NombreProducto VARCHAR(100) NOT NULL,
+    PrecioCompra DECIMAL(10,2) NOT NULL,
+    PrecioProducto DECIMAL(10,2) NOT NULL,
+    CantidadStock INT NOT NULL
+);
+
+-- Tabla Ventas
+CREATE TABLE IF NOT EXISTS Ventas (
+    IDVenta INT AUTO_INCREMENT PRIMARY KEY,
+    Cliente VARCHAR(255),
+    IDEmpleado INT,
+    FechaVenta DATETIME NOT NULL,
+    TotalVenta DECIMAL(10,2) NOT NULL,
+    TotalExtra DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    TotalFinal DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    Estado ENUM('pendiente','finalizada') NOT NULL DEFAULT 'pendiente',
+    MetodoPago ENUM('efectivo','tarjeta','codigo_qr')
+);
+
+-- Tabla DetallesVenta
+CREATE TABLE IF NOT EXISTS DetallesVenta (
+    IDDetalleVenta INT AUTO_INCREMENT PRIMARY KEY,
+    IDVenta INT,
+    IDProducto INT,
+    PrecioUnitario DECIMAL(10,2),
+    CantidadVendida INT NOT NULL
+);
+
+-- Tabla Reportes
+CREATE TABLE IF NOT EXISTS Reportes (
+    IDReporte INT AUTO_INCREMENT PRIMARY KEY,
+    TipoReporte ENUM('diario','semanal','mensual') NOT NULL,
+    Fecha DATE,
+    Mes INT,
+    Anio INT,
+    TotalVentas DECIMAL(10,2) NOT NULL,
+    OtrosDetalles TEXT
+);
+
